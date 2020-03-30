22576d2ae0c1fa156d29832ca0ccfd2bb0649631
chaojimeinv <62868508+chaojimeinv@users.noreply.github.com>
2020-03-30 20:26:53 +0800

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,3 @@
-<?php
-/**
-* iCMS - i Content Management System
-* Copyright (c) 2007-2017 iCMSdev.com. All rights reserved.
-*
-* @author icmsdev <master@icmsdev.com>
-* @site https://www.icmsdev.com
-* @licence https://www.icmsdev.com/LICENSE.html
-*/
-require dirname(__FILE__).'/iCMS.php';
-iCMS::run();
+<script>
+window.location.href='admin.php?action=frame&ctrl=login'
+</script>
